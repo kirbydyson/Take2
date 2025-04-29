dd362c208129d8dc8d028de4dea42ce3cbdf8983
kirbydyson <kdysonsc@gmail.com>
2025-04-29 01:37:23 -0500

    api requests for scoredle

--- server/app.py
+++ server/app.py
@@ -4,6 +4,11 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+@app.route('/test', methods=['GET'])
+def test():
+    return {'message': 'API is working!'}
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
  
