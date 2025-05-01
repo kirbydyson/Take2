6b979e6dd180ee5b2c611115ea581fd9ef6a123d
samuelchoi0522 <jaedolc@gmail.com>
2025-05-01 17:03:03 -0500

    Refactor Homepage component to use VideoBackground component for video rendering

--- client/src/components/Homepage/Homepage.jsx
+++ client/src/components/Homepage/Homepage.jsx
@@ -1,16 +1,11 @@
-import styles from "@/app/page.module.css";
+'use client'
+
+import VideoBackground from "./Videobackground";
 
 export default function Homepage() {
     return (
         <div className='video-container'>
-            <video
-                src='https://storage.googleapis.com/temp_bucket_for_db/baseball%20video.mp4'
-                autoPlay
-                muted
-                loop
-                playsInline
-                className={styles.backgroundVideo}
-            />
+            <VideoBackground />
             <div className='overlay-content'>{/* Add more content here */}</div>
         </div>
     );
---
+++ client/src/components/Homepage/Videobackground.jsx
@@ -0,0 +1,19 @@
+'use client';
+
+import styles from '@/app/page.module.css';
+
+export default function VideoBackground() {
+    return (
+        <div className='video-container'>
+            <video
+                src='https://storage.googleapis.com/temp_bucket_for_db/baseball%20video.mp4'
+                autoPlay
+                muted
+                loop
+                playsInline
+                className={styles.backgroundVideo}
+            />
+            <div className='overlay-content'>{/* Add more content here */}</div>
+        </div>
+    );
+}
